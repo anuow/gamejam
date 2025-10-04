39d7df1adf44a5d366564efaa8ca5e7afe7c770f
Sumit Roy <rishikesh7906@gmail.com>
2025-10-04 14:26:27 +0530

    Update falling_spikes.gd

--- gamejamjse/scripts/falling_spikes.gd
+++ gamejamjse/scripts/falling_spikes.gd
@@ -1,27 +1,30 @@
 extends Node2D
 
-@export var speed = 160.0  # Can be set randomly by the spawner
+@export var speed = 200  # Can be set randomly by the spawner
 var current_speed = 0.0
 
 func _physics_process(delta):
 	# Move based on rotation and speed
 	var velocity = Vector2(0, 1).rotated(rotation) * current_speed
 	position += velocity * delta
-	print("Position: ", position, " | Rotation: ", rotation_degrees, " | Speed: ", current_speed)
+	
+	#print("Position: ", position, " | Rotation: ", rotation_degrees, " | Speed: ", current_speed)
 
 func _on_player_detect_area_entered(area: Area2D) -> void:
-	print("Detected area: ", area.name, " | Parent: ", area.get_parent().name, " | In group: ", area.is_in_group("player"))
+	#print("Detected area: ", area.name, " | Parent: ", area.get_parent().name, " | In group: ", area.is_in_group("player"))
 	if area.is_in_group("player"):
 		print("Player detected, calling fall()")
 		fall()
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
-		print("Hitbox triggered, dealing damage")
+		#print("Hitbox triggered, dealing damage")
 		var parent = area.get_parent()
 		var health_component = parent.find_child("HealthComponent")
 		if health_component:
 			health_component.take_damage(50.0)
+			queue_free()
+			
 
 func fall():
 	print("Falling started, current_speed: ", current_speed)
