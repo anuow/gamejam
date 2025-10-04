1fe1840ad99304ac47a36449d771502df8cb89c7
Sumit Roy <rishikesh7906@gmail.com>
2025-10-04 14:26:33 +0530

    Update bullet.gd

--- gamejamjse/scripts/bullet.gd
+++ gamejamjse/scripts/bullet.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
 @export var speed: float = 200.0
-@export var damage: float = 10.0 # <-- ADDED DAMAGE VARIABLE
+@export var damage: float = 20 
 
 var move_dir: Vector2 = Vector2.ZERO
-var owner_group: String 
+var owner_group: String = ""
 
 @onready var distroy_timer: Timer = $DistroyTimer
 
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 	global_position += move_dir * speed * delta
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Bullet hit: ", body.name) # <-- ADD THIS
+	print("Bullet hit: ", body.name) 
 	if body.is_in_group(owner_group):
 		return
 
@@ -23,7 +23,7 @@ func _on_body_entered(body: Node2D) -> void:
 	# Check if the body we hit has a HealthComponent
 	var health_component = body.find_child("HealthComponent")
 	if health_component:
-		# If it does, call its take_damage function
+		# If it does, call its take_damage functiondddd
 		health_component.take_damage(damage)
 
 	# The bullet is destroyed after hitting anything valid.
