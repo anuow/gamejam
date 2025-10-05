52047c7f6a42ce2410b488767a629661fc939ccc
Rishikesh Roy <rishikesh7906@gmail.com>
2025-10-06 04:06:15 +0530

    Update main_menu.gd

--- gamejamjse/scripts/main_menu.gd
+++ gamejamjse/scripts/main_menu.gd
@@ -4,12 +4,13 @@ extends Control
 @onready var main_buttons: VBoxContainer = $MainButtons
 @onready var options: Panel = $Options
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _ready() -> void:
 	main_buttons.visible = true
 	options.visible = false
+	MusicManager.play_music(preload("res://Music/realmainmusic.ogg"))
 
 
 func _on_start_pressed() -> void:
