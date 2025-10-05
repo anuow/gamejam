93c42674698551349ab3166decf2e2c3e943371f
Rishikesh Roy <rishikesh7906@gmail.com>
2025-10-06 04:06:31 +0530

    Update good_ending.gd

--- gamejamjse/scripts/good_ending.gd
+++ gamejamjse/scripts/good_ending.gd
@@ -4,6 +4,7 @@ func _ready():
 	var good_ending_dialogue = load("res://Dialogue/good_ending.dialogue")
 	DialogueManager.connect("dialogue_ended", Callable(self, "_on_dialogue_ended"))
 	DialogueManager.show_dialogue_balloon(good_ending_dialogue)
+	MusicManager.play_music(preload("res://Music/scene_1.ogg"))
 
 func _on_dialogue_ended(resource: DialogueResource):
 	if resource.resource_path.ends_with("good_ending.dialogue"):
