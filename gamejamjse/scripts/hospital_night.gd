68ec489429f18fdfbe947cf59101e59f1dde6f47
anu <anirudh157290@gmail.com>
2025-10-05 22:39:47 +0530

    a

---
+++ gamejamjse/scripts/hospital_night.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var wife_sprite: AnimatedSprite2D = $WifeAnimatedSprite2D
+@onready var wife_anim_player: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	var wife_dialogue = load("res://Dialogue/HP_Night.dialogue")
+	DialogueManager.show_dialogue_balloon(wife_dialogue)
+	DialogueManager.connect("dialogue_ended", Callable(self, "_on_dialogue_ended"))
+
+func _on_dialogue_ended(resource: DialogueResource):
+	if resource.resource_path.ends_with("HP_Night.dialogue"):
+		_wife_leave()
+		
+	elif resource.resource_path.ends_with("HP_Night2.dialogue"):
+		SceneManager.change_scene("res://scenes/lvl_2.tscn")
+
+func _wife_leave():
+	wife_anim_player.play("leave")
+	await wife_anim_player.animation_finished
+	wife_sprite.visible = false
+	var patient_dialogue = load("res://Dialogue/HP_Night2.dialogue")
+	DialogueManager.show_dialogue_balloon(patient_dialogue)
